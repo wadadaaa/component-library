6ed4405557b87dc850b67fc6548754b5ec033be1
wadadaaa <lopatinski@gmail.com>
2020-05-19 13:33:19 +0300

    first commit

---
+++ src/Components/Buttons.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const PrimaryButton = styled.button`
+  background: red;
+  padding: 14px 24px;
+  color: white;
+  border: none;
+  font-size: 1rem;
+`;
+
+export default PrimaryButton;
