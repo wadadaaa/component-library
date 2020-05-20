8142f282c014e28a57c00ce859a6f62acd70dd44
wadadaaa <lopatinski@gmail.com>
2020-05-20 12:32:11 +0300

    add custom modal and buttons, also themes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
-import PrimaryButton from "./Components/Buttons";
+import {ThemeProvider} from 'styled-components';
+import {
+  SignUpModal
+} from "./Components";
+import {GlobalStyle, darkTheme, defaultTheme} from './utils';
 
 const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = useState(false);
   return (
-    <div>
-      <PrimaryButton>Submit</PrimaryButton>
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button 
+      style={{margin: '0 16px 24px', padding: '8px', background: 'none'}}
+      onClick={() => setUseDarkTheme(true)}>
+        Dark theme
+      </button>
+      <button 
+      style={{margin: '0 16px 24px', padding: '8px', background: 'none'}}
+      onClick={() => setUseDarkTheme(false)}>
+        Default theme
+      </button>
+      <div style={{
+        background: useDarkTheme ? 
+        defaultTheme.primaryColor : 
+        darkTheme.primaryColor,
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around'
+      }}>
+    <SignUpModal />
+      <GlobalStyle />
     </div>
-  );
+    </ThemeProvider>
+  );  
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
