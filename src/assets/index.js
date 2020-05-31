867b77cfa1c40de60ec38dcbfcaa746f9b0cac0a
wadadaaa <lopatinski@gmail.com>
2020-05-31 12:56:29 +0300

    some fix

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,5 @@
-import SignUp from './illustrations/sign-up.svg';
+// import SignUp from './illustrations/sign-up.svg';
+import SignUp from "./illustrations/SignUp.svg";
 export * from "./icons/CloseIcon";
   
 export const Illustrations = {
