ed2b2defe19bcd0ed7d682cbdeb5ef5e8a98a1c8
wadadaaa <lopatinski@gmail.com>
2020-05-31 13:02:40 +0300

    add accessibility

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import { addParameters } from "@storybook/react";
 import { addDecorator } from "@storybook/react";
 import { withContexts } from "@storybook/addon-contexts/react";
 import { withKnobs } from "@storybook/addon-knobs";
+import { withA11y } from "@storybook/addon-a11y";
 import { contexts } from "./contexts";
 
 addParameters({
@@ -12,4 +13,5 @@ addParameters({
 });
 
 addDecorator(withContexts(contexts));
-addDecorator(withKnobs)
+addDecorator(withKnobs);
+addDecorator(withA11y);
